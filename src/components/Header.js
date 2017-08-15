9eb7dc3882738f3773deae8d0126316677124a37
lerning <joshuabenlerner@gmail.com>
2017-08-15 17:43:36 -0600

    inilialized git repo, have begun reactifying the code

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Header extends React.Component {
+   render () {
+      return (
+         <div className="header clearfix">
+           <h3 className="text-muted">Exercise: Full Stack - JavaScript</h3>
+         </div>
+      )
+   }
+}
+
+export default Header
